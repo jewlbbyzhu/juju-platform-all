24a93c852f261ab1d95a339d8f0b110fda7a3098
jewlbbyzhu <jewlbbyzhu@gmail.com>
2026-04-25 21:55:39 +0800

    auto: pre-deploy commit

---
+++ JujuApp/jest-transformer.js
@@ -0,0 +1,4 @@
+const babelJest = require('babel-jest');
+
+// 直接使用 babel-jest 的默认 transformer
+module.exports = babelJest.default;
--- JujuApp/jest.config.js
+++ JujuApp/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-reanimated|react-native-linear-gradient|react-native-worklets|@expo|expo)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
